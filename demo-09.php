1258cd38fabf1c180d5faf8a71a69b2ece24bb4f
J. Albert Bowden II <jalbertbowden@gmail.com>
2013-08-26 04:57:12 -0400

    demo 09
    demo 09 and dummy data document: how to customize parsing via callback
    function(s)

---
+++ demo-09.php
@@ -0,0 +1,18 @@
+<?php
+    include('simple_html_dom.php');
+$html = file_get_html('dummydata03.html'); 
+// Write a function with parameter "$element"
+echo "<h1>Original Paragaph</h1>";
+echo "<p>This paragraph has <b>plenty</b> of <code>b</code> elements; <b>here's one</b>, not here though, <b>here's another</b>, and <b>and this paragraph's fourth</b>.</p>";
+function my_callback($element) {         
+    // Hide all <b> tags          
+    if ($element->tag=='b')                 
+        $element->outertext = ''; 
+}  
+
+// Register the callback function with it's function name 
+$html->set_callback('my_callback'); 
+echo "<h1>Paragraph Sans <code>b</code> Elements</h1>";
+// Callback function will be invoked while dumping 
+echo $html;
+?>
