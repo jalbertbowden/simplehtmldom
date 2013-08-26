9f6439cbb0fc60baab5ec44dd64c5e0cf2d97263
J. Albert Bowden II <jalbertbowden@gmail.com>
2013-08-26 00:03:38 -0400

    demos
    two more demos and a dummy content document

---
+++ demo-04.php
@@ -0,0 +1,4 @@
+<?php
+    include('simple_html_dom.php');
+echo file_get_html('http://www.google.com/')->plaintext;
+?>
---
+++ demo-05.php
@@ -0,0 +1,14 @@
+<?php
+    include('simple_html_dom.php');
+$html = file_get_html('slashdotcopy.html');  
+
+// Find all article blocks  
+foreach($html->find('article') as $article) {      
+    $item['header'] = $article->find('header', 0)->plaintext;      
+    $item['body']    = $article->find('div.body', 0)->plaintext;      
+    $item['footer'] = $article->find('footer', 0)->plaintext;      
+    $articles[] = $item;  
+}  
+
+print_r($articles);
+?>
